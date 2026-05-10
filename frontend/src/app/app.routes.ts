3c92a60130fd1921220a43ee482031fb06190703
L0OKASS <lucraft14738@gmail.com>
2026-05-10 19:37:32 -0400

    feat(frontend): menu principal con protección de rutas (authGuard)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './features/home/pages/home-page/home-page';
 import { LoginPage } from './features/auth/pages/login-page/login-page';
+import { MenuPage } from './features/menu/pages/menu-page/menu-page';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
   { path: 'login', component: LoginPage },
+  { path: 'menu', component: MenuPage, canActivate: [authGuard] },
   { path: '**', redirectTo: '' }
 ];
